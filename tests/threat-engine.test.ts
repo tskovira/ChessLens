5aeb17f267ac9e32425eecf8d609cf06a9b9cc15
Travis Skovira <skoviratravis@gmail.com>
2026-08-19 16:11:25 -0400

    Test legal and pinned defenders

--- tests/threat-engine.test.ts
+++ tests/threat-engine.test.ts
@@ -1,30 +1,36 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { calculateThreats, parseFen } from "../src/threat-engine.js";
+import {calculateThreats,parseFen} from "../src/threat-engine.js";
 
-test("a knight attacking f7 creates level-one pressure", () => {
-  const threats = calculateThreats(
-    parseFen("8/5p2/3N4/8/8/8/8/8 w - - 0 1"),
-  );
-  assert.equal(threats.length, 1);
-  assert.equal(threats[0]?.target.square, "f7");
-  assert.equal(threats[0]?.level, 1);
-  assert.equal(threats[0]?.attackers[0]?.type, "knight");
+test("a knight attacking f7 creates level-one pressure",()=>{
+  const threat=calculateThreats(parseFen("8/5p2/3N4/8/8/8/8/8 w - - 0 1"))[0];
+  assert.equal(threat?.target.square,"f7");
+  assert.equal(threat?.level,1);
+  assert.equal(threat?.attackers[0]?.type,"knight");
 });
 
-test("three attackers create level-three pressure", () => {
-  const threats = calculateThreats(
-    parseFen("3q4/5N2/3R4/6B1/8/8/8/8 w - - 0 1"),
-  );
-  const queen = threats.find(threat => threat.target.square === "d8");
-  assert.equal(queen?.attackers.length, 3);
-  assert.equal(queen?.level, 3);
+test("three attackers create level-three pressure",()=>{
+  const threat=calculateThreats(parseFen("3q4/5N2/3R4/6B1/8/8/8/8 w - - 0 1"))
+    .find(item=>item.target.square==="d8");
+  assert.equal(threat?.attackers.length,3);
+  assert.equal(threat?.level,3);
 });
 
-test("sliding pieces cannot attack through blockers", () => {
-  const threats = calculateThreats(
-    parseFen("3q4/3p4/3R4/8/8/8/8/8 w - - 0 1"),
-  );
-  assert.equal(threats.some(threat => threat.target.square === "d8"), false);
-  assert.equal(threats.find(threat => threat.target.square === "d7")?.attackers[0]?.type, "rook");
+test("a legal recapturing piece is a usable defender",()=>{
+  const threat=calculateThreats(parseFen("6k1/5p2/3N4/8/8/8/8/K7 w - - 0 1"))
+    .find(item=>item.target.square==="f7");
+  assert.equal(threat?.defenders.length,1);
+  assert.equal(threat?.defenders[0]?.type,"king");
+});
+
+test("a pinned piece is not a usable defender",()=>{
+  const threat=calculateThreats(parseFen("R4n1k/8/4p3/3P4/8/8/8/K7 w - - 0 1"))
+    .find(item=>item.target.square==="e6");
+  assert.equal(threat?.defenders.length,0);
+});
+
+test("sliding pieces cannot attack through blockers",()=>{
+  const threats=calculateThreats(parseFen("3q4/3p4/3R4/8/8/8/8/8 w - - 0 1"));
+  assert.equal(threats.some(threat=>threat.target.square==="d8"),false);
+  assert.equal(threats.find(threat=>threat.target.square==="d7")?.attackers[0]?.type,"rook");
 });
